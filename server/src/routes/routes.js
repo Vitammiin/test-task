6942a21e16f9939721cc71e7aa7eaea6e50d0864
Volodymyr <davidvovchik@gmail.com>
2024-11-26 14:11:17 +0300

    fix

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,10 +1,7 @@
-import { getAudio, postAudio } from '../controllers/audio.controller.js';
 import { postEmails } from '../controllers/email.controller.js';
 import { getStock } from '../controllers/stock.controller.js';
 
 export default function routes(server) {
-    server.get('/audio', getAudio);
-    server.post('/audio', postAudio);
 
     server.post('/emails', postEmails);
 
--- server/src/utils/mongo.js
+++ server/src/utils/mongo.js
@@ -1,7 +1,7 @@
 import Fastify from 'fastify';
-import fastifyMongo from '@fastify/mongodb';
-import dotenv from 'dotenv';
 import fastifyCors from '@fastify/cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -13,9 +13,19 @@ server.register(fastifyCors, {
     allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
-server.register(fastifyMongo, {
-    url: process.env.MONGO_URI,
-    database: process.env.DB_NAME,
-});
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB via Mongoose');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    }
+};
+
+connectToDatabase();
 
 export default server;
