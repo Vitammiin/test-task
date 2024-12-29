0a6cfe7ed3a3994f28ec67fbc4f931694150492b
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-12-29 17:28:38 +0000

    url fix 2

--- frontend/src/api/apiStock.ts
+++ frontend/src/api/apiStock.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getStock = async (country: string, symbol: string) => {
   try {
     const response = await axios.get(
-      `https://audio-recorder-backend-plno.onrender.com/stock?country=${country}&symbol=${symbol}`,
+      `http://127.0.0.1:3001/stock?country=${country}&symbol=${symbol}`,
     );
     if (response && response.data) {
       return response.data;
@@ -16,7 +16,7 @@ export const getStock = async (country: string, symbol: string) => {
 export const getAllStock = async (country: string, symbols: string[]) => {
   try {
     const response = await axios.get(
-      `https://audio-recorder-backend-plno.onrender.com/stocks?country=${country}&symbols=${symbols.join(',')}`,
+      `http://127.0.0.1:3001/stocks?country=${country}&symbols=${symbols.join(',')}`,
     );
     if (response && response.data) {
       return response.data;
